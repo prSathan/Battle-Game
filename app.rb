b99a1ba881da6552736bc42d2e11e4a6baa6ce8e
Prabu Sathan <prabu.sathan@gmail.com>
2017-05-23 12:46:43 +0100

    params

--- app.rb
+++ app.rb
@@ -14,7 +14,13 @@ get '/secret' do
   'the secret page!'
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
