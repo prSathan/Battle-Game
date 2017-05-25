c794bf922f6624077abaa12b03567d63f695c5e5
Colin Scally <colin.scally@gmail.com>
2017-05-25 15:53:44 +0100

    refactor

--- battle/spec/game_spec.rb
+++ battle/spec/game_spec.rb
@@ -3,6 +3,15 @@ require 'game'
 describe Game do
   subject(:game) { described_class.new('p1', 'p2') }
 
+  it 'initialises with two arguments' do
+    expect(game).to be_an_instance_of(Game)
+  end
+
+  it 'creates instance of player class' do
+    expect(game.player_1).to be_an_instance_of(Player)
+    expect(game.player_2).to be_an_instance_of(Player)
+  end
+
   describe '#attack' do
     it 'attacks a player' do
       expect(game.player_2).to receive(:reduce_hp)
--- battle/spec/spec_helper.rb
+++ battle/spec/spec_helper.rb
@@ -3,6 +3,17 @@ require 'capybara/rspec'
 require 'rspec'
 require 'features/web_helpers'
 
+require 'simplecov'
+require 'simplecov-console'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
+  # Want a nice code coverage website? Uncomment this next line!
+  # SimpleCov::Formatter::HTMLFormatter
+])
+
+SimpleCov.start
+
 ENV['RACK_ENV'] = 'test'
 
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
