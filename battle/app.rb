ce3d5042d75f6bffb6de8bea880c9efbdd1d6eed
Prabu Sathan <prabu.sathan@gmail.com>
2017-05-25 15:34:44 +0100

    Got sinatara working with game class accepting two parameters, tests til failing

--- battle/app.rb
+++ battle/app.rb
@@ -12,27 +12,20 @@ class Battle < Sinatra::Base
   end
 
  post '/names' do
-   $player_1 = Player.new(params[:player1])
-   $player_2 = Player.new(params[:player2])
+   $game = Game.new(params[:player1],params[:player2])
    redirect to('/play')
  end
 
  get '/play' do
-   @player_1_name = $player_1.name
-   @player_2_name = $player_2.name
    erb(:play)
  end
 
  post '/hitpoints' do
-   @player_1_points = $player_1.hp
-   @player_2_points = $player_2.hp
    erb(:hitpoints)
  end
 
  get '/attack' do
-   @player_1 = $player_1
-   @player_2 = $player_2
-   Game.new.attack(@player_2)
+   $game.attack($game.player_2)
    erb(:attack)
  end
 
--- battle/lib/game.rb
+++ battle/lib/game.rb
@@ -2,6 +2,13 @@ require_relative 'player'
 
 class Game
 
+  attr_reader :player_1, :player_2
+
+  def initialize(player1, player2)
+    @player_1 = Player.new(player1)
+    @player_2 = Player.new(player2)
+  end
+
   def attack(player)
     player.reduce_hp
   end
