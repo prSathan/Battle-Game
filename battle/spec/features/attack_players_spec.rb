ffbfacadb5064ef385e5cf059ba4166949b24f3c
Prabu Sathan <prabu.sathan@gmail.com>
2017-05-26 12:00:51 +0100

    Multiplayer

--- battle/spec/features/attack_players_spec.rb
+++ battle/spec/features/attack_players_spec.rb
@@ -1,10 +1,19 @@
 feature 'Attack player' do
 
 
-  scenario 'attack reduces hp by 10' do
+  scenario 'Player 1\'s attack reduces Player 2\'s hp by 10' do
     sign_in_and_play
     click_button('ATTACK!')
     click_button("Bob's Turn")
+    expect(page).to have_content 'Points: 90'
+  end
+
+  scenario 'Player 2\'s attack reduces Player 1\'s hp by 10' do
+    sign_in_and_play
+    click_button('ATTACK!')
+    click_button("Bob's Turn")
+    click_button('ATTACK!')
+    click_button("Alice's Turn")
     expect(page).to have_content 'Points: 90'
   end
 
@@ -31,5 +40,12 @@ feature 'Attack player' do
 
   scenario 'player 2 attacks player 1 and receives confirmation' do
     sign_in_and_play
+    click_button('ATTACK!')
+    click_button("Bob's Turn")
+    click_button('ATTACK!')
+    expect(page).to have_content 'You have successfully attacked Alice'
   end
+
+
+
 end
